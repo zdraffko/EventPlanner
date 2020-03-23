9e4fe48425d27e9ebfc2b42606d0cf4e82238796
zdraffko <zdravkomb1@gmail.com>
2020-03-23 14:40:41 +0200

    Implemented get all events query and added mediatr

---
+++ Application/Events/Queries/GetAllEventsList/GetAllEventsListHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using Application.Common.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Events.Queries.GetAllEventsList
+{
+    public class GetAllEventsListHandler : IRequestHandler<GetAllEventsListQuery, IList<Event>>
+    {
+        private readonly IEventPlannerDbContext _context;
+
+        public GetAllEventsListHandler(IEventPlannerDbContext context) => _context = context;
+
+        public async Task<IList<Event>> Handle(GetAllEventsListQuery request, CancellationToken cancellationToken)
+            => await _context.Events.ToListAsync(cancellationToken);
+    }
+}
---
+++ Application/Events/Queries/GetAllEventsList/GetAllEventsListQuery.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+using Domain.Entities;
+using MediatR;
+
+namespace Application.Events.Queries.GetAllEventsList
+{
+    public class GetAllEventsListQuery : IRequest<IList<Event>> { }
+}
