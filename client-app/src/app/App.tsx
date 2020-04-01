40d854e92bcaffa14f93956883cf93eae3108794
zdraffko <zdravkomb1@gmail.com>
2020-04-01 19:17:52 +0300

    Added react router

--- client-app/src/app/App.tsx
+++ client-app/src/app/App.tsx
@@ -1,10 +1,13 @@
 import React, { useEffect, useContext } from "react";
+import { Route } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { Container } from "semantic-ui-react";
 import eventStore from "./stores/eventStore";
 import NavBar from "./components/layout/NavBar";
 import EventsDashboard from "./components/events/dashboard/EventsDashboard";
 import LoaderComponent from "./components/layout/LoaderComponent";
+import Home from "./components/layout/Home";
+import EventForm from "./components/events/EventForm";
 
 const App: React.FC = () => {
   const { loadAllEvents, isGlobalLoading } = useContext(eventStore);
@@ -19,7 +22,9 @@ const App: React.FC = () => {
     <>
       <NavBar />
       <Container style={{ marginTop: "7em" }}>
-        <EventsDashboard />
+        <Route exact path="/" component={Home} />
+        <Route path="/events" component={EventsDashboard} />
+        <Route path="/createEvent" component={EventForm} />
       </Container>
     </>
   );
---
+++ client-app/src/app/components/layout/Home.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Container } from "semantic-ui-react";
+
+const Home = () => (
+  <Container style={{ marginTop: "7em" }}>
+    <h1>Welcome</h1>
+  </Container>
+);
+
+export default Home;
